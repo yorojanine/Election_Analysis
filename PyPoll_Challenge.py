5bc584dee3ce89ba4144f48ee54334548307911b
yorojanine <j9yoro@gmail.com>
2020-12-02 23:29:09 -0800

    updates to txt name

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -9,7 +9,8 @@ import os
 #file_to_load = os.path.join("../Resources/election_results.csv")
 file_to_load = os.path.join("resources","election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+#file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
